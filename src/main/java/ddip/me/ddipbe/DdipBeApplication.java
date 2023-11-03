21879c6590ccc99020cc6b0e4f4429253764ff41
gongmeda <gongmeda@gmail.com>
2023-11-03 12:05:51 +0900

    init

---
+++ src/main/java/ddip/me/ddipbe/DdipBeApplication.java
@@ -0,0 +1,13 @@
+package ddip.me.ddipbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdipBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdipBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ddip/me/ddipbe/DdipBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ddip.me.ddipbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdipBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
