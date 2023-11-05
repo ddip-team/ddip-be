0ae420a541e6ebe50f0a34653c4c6d77706e803f
gongmeda <gongmeda@gmail.com>
2023-11-05 10:33:20 +0900

    chore: 불필요한 import 제거

--- src/main/java/ddip/me/ddipbe/domain/Permit.java
+++ src/main/java/ddip/me/ddipbe/domain/Permit.java
@@ -6,8 +6,6 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
