4fb21a4f0313798fb08312615c4b616256999cdd
seonghoo1217 <seonghoo1217@naver.com>
2023-11-06 23:26:10 +0900

    refactor : API 응답 성공시 code 값 null처리

--- src/main/java/ddip/me/ddipbe/presentation/EventAPI.java
+++ src/main/java/ddip/me/ddipbe/presentation/EventAPI.java
@@ -3,12 +3,9 @@ package ddip.me.ddipbe.presentation;
 import ddip.me.ddipbe.application.EventService;
 import ddip.me.ddipbe.global.annotation.SessionMemberId;
 import ddip.me.ddipbe.global.dto.ResponseEnvelope;
-import ddip.me.ddipbe.global.util.SessionUtil;
 import ddip.me.ddipbe.presentation.dto.request.EventCreateReqDTO;
 import ddip.me.ddipbe.presentation.dto.response.EventCommonResDTO;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,21 +17,22 @@ import java.util.UUID;
 public class EventAPI {
 
     private final EventService eventService;
+
     @PostMapping
-    public ResponseEnvelope<?> createNovelEvent(@RequestBody EventCreateReqDTO eventCreateReqDTO,@SessionMemberId Long memberId){
+    public ResponseEnvelope<?> createNovelEvent(@RequestBody EventCreateReqDTO eventCreateReqDTO, @SessionMemberId Long memberId) {
         EventCommonResDTO novelEvent = eventService.createNovelEvent(eventCreateReqDTO, memberId);
-        return new ResponseEnvelope<>("success",novelEvent,null);
+        return new ResponseEnvelope<>(null, novelEvent, null);
     }
 
     @GetMapping("/{uuid}")
-    public ResponseEnvelope<?> findEventByUuid(@PathVariable UUID uuid){
+    public ResponseEnvelope<?> findEventByUuid(@PathVariable UUID uuid) {
         EventCommonResDTO findEvent = eventService.findEventByUuid(uuid);
-        return new ResponseEnvelope<>("success",findEvent,null);
+        return new ResponseEnvelope<>(null, findEvent, null);
     }
 
     @GetMapping("/me")
-    public ResponseEnvelope<?> findOwnEvent(@SessionMemberId Long memberId){
+    public ResponseEnvelope<?> findOwnEvent(@SessionMemberId Long memberId) {
         List<EventCommonResDTO> ownEvents = eventService.findOwnEvent(memberId);
-        return new ResponseEnvelope<>("success",ownEvents,null);
+        return new ResponseEnvelope<>(null, ownEvents, null);
     }
 }
