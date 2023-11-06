546709e66fbe4bb6d29ad56ebe8d6d957ce0c48c
seonghoo1217 <seonghoo1217@naver.com>
2023-11-06 19:14:31 +0900

    test: 이벤트 생성 시 유효하지 못한 회원 ID 예외 테스트 @DisplayName 추가

--- src/test/java/ddip/me/ddipbe/application/EventServiceTest.java
+++ src/test/java/ddip/me/ddipbe/application/EventServiceTest.java
@@ -3,6 +3,7 @@ package ddip.me.ddipbe.application;
 import ddip.me.ddipbe.application.exception.NotFoundIdException;
 import ddip.me.ddipbe.presentation.dto.request.EventCreateReqDTO;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,6 +21,7 @@ public class EventServiceTest {
     private EventService eventService;
 
     @Test
+    @DisplayName("이벤트 생성 시 유효하지 못한 회원 ID 입력 예외 테스트")
     void EventCreateByFormattingValueExcepted_InvalidMemberId(){
         //given
         Long memberId = 9999L;
