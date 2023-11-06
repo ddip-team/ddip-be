2f5ef13bc4170164c14e6dcd667f41cdded57f95
seonghoo1217 <seonghoo1217@naver.com>
2023-11-06 19:21:34 +0900

    test: 이벤트 조회시 유효하지 않은 UUID 값 입력시 예외 테스트

--- src/test/java/ddip/me/ddipbe/application/EventServiceTest.java
+++ src/test/java/ddip/me/ddipbe/application/EventServiceTest.java
@@ -1,6 +1,7 @@
 package ddip.me.ddipbe.application;
 
 import ddip.me.ddipbe.application.exception.NotFoundIdException;
+import ddip.me.ddipbe.application.exception.NotFoundUuidException;
 import ddip.me.ddipbe.presentation.dto.request.EventCreateReqDTO;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -9,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 import static java.time.LocalDateTime.*;
 import static org.assertj.core.api.Assertions.*;
@@ -22,7 +24,7 @@ public class EventServiceTest {
 
     @Test
     @DisplayName("이벤트 생성 시 유효하지 못한 회원 ID 입력 예외 테스트")
-    void EventCreateByFormattingValueExcepted_InvalidMemberId(){
+    void eventCreateByFormattingValueExcepted_InvalidMemberId(){
         //given
         Long memberId = 9999L;
 
@@ -31,6 +33,19 @@ public class EventServiceTest {
                 eventService.createNovelEvent(initializingCreateDTO(),memberId);
             }
                 ).isInstanceOf(NotFoundIdException.class);
+    }
+
+    @Test
+    @DisplayName("이벤트 조회시 유효하지 않은 UUID값 예외 테스트")
+    void findEventByInvalidUuid(){
+        //given
+        UUID targetUuid = UUID.randomUUID();
+
+        //when
+        assertThatThrownBy(()->{
+                eventService.findEventByUuid(targetUuid);
+            }
+                ).isInstanceOf(NotFoundUuidException.class);
     }
 
 
