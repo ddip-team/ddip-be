d6603526962a5d763a35727effd35870dfc17b71
seonghoo1217 <seonghoo1217@naver.com>
2023-11-14 04:10:10 +0900

    refactor: EventSuccessRes successImageUrl 필드 값 네이밍 변경 및 final 처리

--- src/main/java/ddip/me/ddipbe/presentation/dto/response/EventSuccessRes.java
+++ src/main/java/ddip/me/ddipbe/presentation/dto/response/EventSuccessRes.java
@@ -1,17 +1,19 @@
 package ddip.me.ddipbe.presentation.dto.response;
 
 import ddip.me.ddipbe.domain.Event;
+import lombok.Getter;
 
 import java.util.Map;
 
+@Getter
 public class EventSuccessRes {
-    private String successContent;
-    private String successImgUrl;
-    private Map<String, String> jsonString;
+    private final String successContent;
+    private final String successImageUrl;
+    private final Map<String, String> jsonString;
 
     public EventSuccessRes(Event event) {
         this.successContent = event.getSuccessContent();
-        this.successImgUrl = event.getSuccessImgUrl();
-        this.jsonString = event.getJsonString();
+        this.successImageUrl = event.getSuccessImageUrl();
+        this.jsonString = event.getSuccessInputInfo();
     }
 }
