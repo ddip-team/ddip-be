36d37a10b44f74ecb96c6eae34b7c22efe7ae28e
seonghoo1217 <seonghoo1217@naver.com>
2023-11-13 20:44:35 +0900

    fix: JsonConverter null값 parsing 시 에러 발생 이슈 해결

--- src/main/java/ddip/me/ddipbe/global/util/JsonConverter.java
+++ src/main/java/ddip/me/ddipbe/global/util/JsonConverter.java
@@ -8,6 +8,7 @@ import jakarta.persistence.AttributeConverter;
 import jakarta.persistence.Converter;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
 @Converter
@@ -18,7 +19,6 @@ public class JsonConverter implements AttributeConverter<Map<String,String>,Stri
     @Override
     public String convertToDatabaseColumn(Map<String, String> attribute) {
         try {
-
             return objectMapper.writeValueAsString(attribute);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Error converting Map to JSON", e);
@@ -28,6 +28,7 @@ public class JsonConverter implements AttributeConverter<Map<String,String>,Stri
     @Override
     public Map<String, String> convertToEntityAttribute(String successFormat) {
         try {
+            if (successFormat == null) return new HashMap<String, String>();
             return objectMapper.readValue(successFormat, new TypeReference<Map<String, String>>(){});
         } catch (IOException e) {
             throw new RuntimeException("Error converting JSON to Map", e);
