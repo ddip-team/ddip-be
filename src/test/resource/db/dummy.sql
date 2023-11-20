f6c83b8ef27337ed5ce6e667e2296aa7d2f3bab1
seonghoo1217 <seonghoo1217@naver.com>
2023-11-20 19:03:37 +0900

    refactor: mysql 프로시저 필수 이벤트 필드값 더미 데이터 생성시 기본 추가

--- src/test/resource/db/dummy.sql
+++ src/test/resource/db/dummy.sql
@@ -9,7 +9,10 @@ BEGIN
     DECLARE START_DATE DATETIME;
     DECLARE END_DATE DATETIME;
     DECLARE EVENT_ID BIGINT;
+    DECLARE THUMBNAIL_IMAGE_URL VARCHAR(255);
     DECLARE TOKEN VARCHAR(255);
+    DECLARE EVENT_CREATE_AT DATETIME;
+    DECLARE SUCCESS_RECORD_CREATE_AT DATETIME;
 
     WHILE M <= 1000
         DO
@@ -18,6 +21,8 @@ BEGIN
             WHILE I <= 5
                 DO
                     SET UUID := UNHEX(REPLACE(UUID(), '-', ''));
+                    SET EVENT_CREATE_AT := NOW();
+                    SET THUMBNAIL_IMAGE_URL := 'https://github.com/seonghoo1217/learning_locust/assets/39437170/e3c03e35-52f1-4146-b4d6-90e3c3f7c1e9';
                     IF I = 1 THEN -- 이벤트 진행중
                         SET START_DATE := NOW() - INTERVAL 1 DAY;
                         SET END_DATE := NOW() + INTERVAL 1 DAY;
@@ -29,16 +34,17 @@ BEGIN
                         SET END_DATE := NOW() + INTERVAL 2 DAY;
                     END IF;
                     INSERT INTO Event(uuid, title, limit_count, remain_count, success_content, start_date_time,
-                                      end_date_time, member_id)
+                                      end_date_time, member_id, created_at, thumbnail_image_url)
                     VALUES (UUID, CONCAT('Event ', (M - 1) * 5 + I), 10, IF(I = 1, 10, IF(I <= 4, 0, 10)), 'Success',
-                            START_DATE, END_DATE, M);
+                            START_DATE, END_DATE, M, EVENT_CREATE_AT, THUMBNAIL_IMAGE_URL);
                     SET EVENT_ID := LAST_INSERT_ID();
                     IF I <= 4 THEN -- 만료된 이벤트에 대해서만 SuccessRecord를 생성
                         SET J := 1;
                         WHILE J <= 10
                             DO
                                 SET TOKEN := (SELECT UUID());
-                                INSERT INTO success_record(token, event_id) VALUES (TOKEN, EVENT_ID);
+                                SET SUCCESS_RECORD_CREATE_AT = NOW();
+                                INSERT INTO success_record(token, event_id, created_at) VALUES (TOKEN, EVENT_ID, SUCCESS_RECORD_CREATE_AT);
                                 SET J := J + 1;
                             END WHILE;
                     END IF;
