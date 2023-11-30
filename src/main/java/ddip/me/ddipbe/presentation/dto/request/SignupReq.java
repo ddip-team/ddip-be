265697594d1afe3b195ee7b88c6fc964a8c83684
gongmeda <gongmeda@gmail.com>
2023-11-30 14:22:45 +0900

    feat: signup API 비밀번호 validation 추가

--- src/main/java/ddip/me/ddipbe/presentation/dto/request/SignupReq.java
+++ src/main/java/ddip/me/ddipbe/presentation/dto/request/SignupReq.java
@@ -2,6 +2,9 @@ package ddip.me.ddipbe.presentation.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record SignupReq(@NotBlank @Email String email, @NotBlank String password) {
+public record SignupReq(
+        @NotBlank @Email String email,
+        @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,20}$") @NotBlank String password) {
 }
